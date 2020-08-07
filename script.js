1d6b7b7d2ba4fdd75ac3d69519142ec0beb2b9f2
unknown <rtfm@seznam.czm>
2020-08-07 12:16:21 +0200

    lekce 28 hotova

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('testing2');
+
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+
+async function selectMediaStream(){
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadeddata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // Catch Error Here
+        console.log('whoops, error here:', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+selectMediaStream();
